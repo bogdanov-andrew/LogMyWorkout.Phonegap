f5f2ada51ed2c90287ddb4743b46ebe2c73f5dc0
bogdanov-andrew <bogdanov.andrew.i@gmail.com>
2015-04-02 00:47:46 +0300

    Log results page

--- www/js/logresults.js
+++ www/js/logresults.js
@@ -1,40 +1,88 @@
 (function(){ 
 	var app = angular.module('logResultsPage',[ ]);
 	app.controller('LogResultsPageController', function(){
-		this.trainData = exerciseList;
+		this.repetitionList = repetitionList;
 		$('#myButton').on('click', function () {
 			window.location.href = "exercises.html";
 		  });
-		$('#addExerciseModal').on('shown.bs.modal', function(){
-			$('#exercise-name').focus();
+		$('#addExerciseRepetitionModal').on('shown.bs.modal', function(){
+			$('#repetition-weight').focus();
 		});	 
 		this.doExercise = function(number){
 		
 		};
-		this.saveExercise = function(){
-			var form = this.addExerciseForm;
-			var nameVal = $('#exercise-name').val();
-			var descriptionVal =  $('#description-text').val();
-			var newExercise = {
-				description: descriptionVal,
-				name: nameVal,
-				number: 0
+		
+		function Timer(timeout){
+			this.timeout = timeout;
+			this.timerPosition = timeout;
+			
+			this.onTimer = function() {
+				$('#timerButton').button('loading');	
+				if(this.timerPostition-1 > 0){
+					this.timerPostition--;
+					$('#timerButton').button('Seconds left: ' + this.timerPostition);
+				}else{
+					clearInterval(this.timer);
+					$('#timerButton').button('reset');	
+				}
 			};
-			this.trainData.push(newExercise);
-			$("#addExerciseModal").modal("hide");
-		}
+		};
+		
+		this.timer = null;
+		
+		this.startTimer = function() {
+			var newTimer = new Timer(15);
+			this.timer = setInterval(
+				function() 
+				{ 
+					newTimer.onTimer(); 
+				}, 1000);
+		};
+				
+		this.logRepetition = function(){
+			var weight = $('#repetition-weight').val();
+			var count = $('#repetition-count').val();
+			var newRepetition = {
+				id: 5,
+				exerciseId: 1,
+				weight: weight,
+				repetitions: count
+			};
+			this.repetitionList.push(newRepetition);
+			$("#addExerciseRepetitionModal").modal("hide");
+		};
 	});
-	var exerciseList = 
+	var repetitionList = 
 		 [
 			{
 				id: 1,
-				name: 'Pop up',
-				description: '10-12-13'
+				exerciseId: 1,
+				weight: 50,
+				repetitions: 10,
 			},
 			{
 				id: 2,
-				name: 'Push up',
-				description: '10-12-13'
+				exerciseId: 1,
+				weight: 52,
+				repetitions: 15,
+			},
+			{
+				id: 3,
+				exerciseId: 1,
+				weight: 53,
+				repetitions: 9,
+			},
+			{
+				id: 4,
+				exerciseId: 1,
+				weight: 55,
+				repetitions: 7,
+			},
+			{
+				id: 4,
+				exerciseId: 1,
+				weight: 55,
+				repetitions: 7,
 			}
 		];
 })();

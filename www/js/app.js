090390193129cf5a9a9e9f613edb14a54712f64b
bogdanov-andrew <bogdanov.andrew.i@gmail.com>
2015-05-06 23:49:14 +0300

    attached SQLite plugin and run DB

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,39 @@
 (function(){ 
 	var app = angular.module('startPage',[ ]);
 	app.controller('StartPageController', function(){
+
+		if( window.cordova ) {
+			document.addEventListener( 'deviceready', start, false );
+		} else {
+			$( start );
+		}
+
+		function start() {
+			// Здесь начинается JS-логика нашего приложения
+			console.log('Init!');
+
+			var db = window.openDatabase("TestDB","1.0", "Demo", -1);
+
+			db.transaction(function(tx) {
+				tx.executeSql('DROP TABLE IF EXISTS test_table');
+				tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+			});
+
+			db.transaction(function(tx) {
+				tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+					console.log("insertId: " + res.insertId + " -- probably 1");
+					console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+				});
+			});
+
+			db.transaction(function(tx) {
+				tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+					console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+					console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+				});
+			});
+		}
+
 		this.trainData = exerciseList;
 		$('#myButton').on('click', function () {
 			window.location.href = "exercises.html";
--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,34 @@
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
+
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        var db = window.openDatabase({name: "DB"});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        db.transaction(function(tx) {
+            tx.executeSql('DROP TABLE IF EXISTS test_table');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+        });
 
-        console.log('Received Event: ' + id);
+        db.transaction(function(tx) {
+            tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+                console.log("insertId: " + res.insertId + " -- probably 1");
+                console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+            });
+        });
+
+        db.transaction(function(tx) {
+            tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+                console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+                console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+            });
+        });
     }
 };
